bf733754c249f1aadbdd589cdcf5fce0e5874352
Ryan <rrobertson@gmail.com>
2019-07-06 10:15:10 -0700

    move tb to opencv

--- src/testbench.cpp
+++ src/testbench.cpp
@@ -1,21 +1,31 @@
 #include "sobel_filter.h"
 #include "ap_fixed.h"
+#include "ap_int.h"
 #include "hls_stream.h"
 #include "ap_axi_sdata.h"
 #include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "hls_opencv.h"
 
 int main (void) {
-	pix_s pixels;
-	pix_s ret;
+    pix_s inStream, outStream;
 
-	pix_t tmp;
-	tmp = 14.2;
-	pixels.write(tmp);
+    // import image as cv matrix
+    cv::Mat inMat = cv::imread("C:/projects/sobel_filter/airplane.bmp");
+    assert(inMat.empty() != 1);
 
-	sobel_filter(pixels, ret);
+    // convert from cv matrix to hls stream
+    cvMat2AXIvideo(inMat, inStream);
 
-	tmp = ret.read();
-    printf("Got %f out\n", tmp.to_float());
+    // pass image to module
+    sobel_filter(inStream, outStream);
 
-	return 0;
+    // convert output stream to cv matrix
+    cv::Mat outMat(512, 512, CV_8UC3);
+    AXIvideo2cvMat(outStream, outMat);
+
+    // write image to disk
+    cv::imwrite("C:/projects/sobel_filter/airplane_dut.bmp", outMat);
+
+    return 0;
 }
